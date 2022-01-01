1ebe6a97e881ab22e4891c70ad58be7c9ce0cd2b
zchandikaz <zchandikaz@gmail.com>
2022-01-01 12:35:13 +0530

    removed old raw html files

--- devtools.js
+++
@@ -1,5 +0,0 @@
-console.log("hello from devtools");
-chrome.devtools.panels.create("ReqEditor",
-    "res/img/logo-16_x_16.png",
-    "req-editor/dev-panel-tab.html",
-    function(panel) { console.log("hello from callback"); });
